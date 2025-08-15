7c70f88250936cb76451ca5f7ef512dd8e94c7cf
Nishit Mistry <nishitmistry94@gmail.com>
2025-08-15 18:03:08 +0000

    excercies from github repo guide

--- boot_sect_stack.asm
+++ boot_sect_stack.asm
@@ -1,31 +1,42 @@
-mov ah, 0x0e
-mov bp, 0x8000 ; this is the address way away from 0x7c00 where out bootsector is loaded by bios
-mov sp, bp
+mov ah, 0x0e ; tty mode
+
+mov bp, 0x8000 ; this is an address far away from 0x7c00 so that we don't get overwritten
+mov sp, bp ; if the stack is empty then sp points to bp
 
 push 'A'
 push 'B'
 push 'C'
 
-mov al, [0x7ffa]
+; to show how the stack grows downwards
+mov al, [0x7ffe] ; 0x8000 - 2
 int 0x10
 
-mov al, [0x801e]
+; however, don't try to access [0x8000] now, because it won't work
+; you can only access the stack top so, at this point, only 0x7ffe (look above)
+mov al, [0x8000]
 int 0x10
 
-pop cx 
-mov al, cl
-int 0x10
+
+; recover our characters using the standard procedure: 'pop' in bx register 
+; We can only pop full words so we need an auxiliary register to manipulate
+; the lower byte
+pop bx
+mov al, bl
+int 0x10 ; prints C
 
 pop bx
 mov al, bl
-int 0x10
+int 0x10 ; prints B
 
 pop bx
 mov al, bl
+int 0x10 ; prints A
+
+; data that has been pop'd from the stack is garbage now
+mov al, [0x8000]
 int 0x10
+
 
 jmp $
-
-; zero padding and magic bios number
 times 510-($-$$) db 0
 dw 0xaa55
